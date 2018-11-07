27e6be593f940bf469dd94d46972125ecdfa4fe2
Emmanuel Lataste <e.lataste@rubika-edu.com>
2018-11-07 17:26:07 +0100

    FirstPush

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public LayerMask groundLayer; // Think about adding it into the scene and to create the corresponding layer : groundLayer
+    private float horizontal;
+    private float vertical;
+    public float speed = 5f;
+    public float jump = 1;
+    public Vector3 groundDistance;
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal"); // Stock left joystick's values in two variables (between -1 and 1)
+        vertical = Input.GetAxis("Vertical");
+
+        Rotation();
+        Movements();
+        Jump();
+
+    }
+
+    void Rotation()
+    {
+
+        if (horizontal != 0 || vertical != 0) // Si on utilise le joystick
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, Mathf.Atan2(-vertical, horizontal) * Mathf.Rad2Deg, 0));
+            // We give to the player a new rotation to his Y, the formula is : ArcTan(-vertical / horizontal)
+            // MathF.Rad2Deg Is to give the value in degrees as it is in radians.
+        }
+    }
+
+    void Movements()
+    {
+        if (horizontal != 0 || vertical != 0)
+        {
+            //GetComponent<Rigidbody>().MovePosition(transform.position + transform.right);
+
+            //GetComponent<Rigidbody>().AddForce(transform.right * speed);
+            GetComponent<Rigidbody>().velocity = (new Vector3(horizontal, 0, vertical) * speed);
+            // Accelerate the player.
+            // transform.Translate(new Vector3(speed, 0, 0));
+        }
+    }
+
+    void Jump()
+    {
+        if (isGrounded() == true) // If the method beneef is true, so if the ray touches the ground
+        {
+            if (Input.GetButtonDown("Jump")) // If we press on Jump
+            {
+                Debug.Log("Jump");
+                GetComponent<Rigidbody>().AddForce(new Vector3(0, jump, 0));
+                // So we add a force to the Y to jump.
+            }
+
+        }
+
+    }
+
+    bool isGrounded() // A methode sending back a boolean.
+    {
+
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, groundDistance, Color.red); // Allow to see the ray in the scene when it's playing.
+        if (Physics.Raycast(transform.position, groundDistance, out hit, 2f, groundLayer))
+        // If a ray of 2f from the player position, oriented towards the ground (groundDistance) touches an object having the layer "ground" then ...
+        {
+            Debug.Log(hit.transform.name);
+            return true; //Then we send back True
+
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.AI;
+
+
+public class EnemyController : MonoBehaviour {
+
+    public bool isReversed = false;
+    public Transform[] waypoints;
+
+    private NavMeshAgent entityAgent;
+    private int destPoint = 0;
+    private int side = 0;
+
+
+    void OnInspectorGUI() {
+        //UI of the inspector for the checkbox for Reversion Path
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Reversion Path", GUILayout.Width(70));
+        isReversed = EditorGUILayout.Toggle(isReversed);
+        GUILayout.EndHorizontal();
+ 
+    }
+
+
+    void Start() {
+        entityAgent = GetComponent < NavMeshAgent>();
+
+        //Without auto-barking the agent has continuous movment, the agent doesn't slow down when getting close to its destination point
+        entityAgent.autoBraking = false;
+
+        ToNextWaypoint();
+    }
+
+
+    void ToNextWaypoint() {
+        //In case of no attribuated waypoint : return
+        if (waypoints.Length == 0) {
+            return;
+        }
+
+        //Say to the agent to go to this currently selected waypoint
+        entityAgent.destination = waypoints[destPoint].position;
+
+        //Define the destination point of the agent
+        //Cycling to start if necessary
+        if (isReversed == false) {
+            destPoint = (destPoint + 1) % waypoints.Length;
+        }
+
+        if (isReversed == true) {
+            if (destPoint == waypoints.Length - 1) {
+                side = 0;
+            }
+
+            if (destPoint == 0) {
+                side = 1;
+            }
+
+            if (side == 0) {
+                destPoint = destPoint - 1;
+            }
+
+            if (side == 1) {
+                destPoint = destPoint + 1;
+            }
+            
+        }
+        
+
+    }
+
+
+    void Update() {
+       
+        //define the next destination point
+        if (!entityAgent.pathPending && entityAgent.remainingDistance < 0.5f) {
+            ToNextWaypoint();
+        }
+
+    }
+
+
+
+}
+
+
+/*
+//Allow for a list of Waypoints
+[System.Serializable]
+public class Waypoints {
+    //solution de scours: public Transform WaypointPosition;
+    public GameObject Waypoint;
+}
+*/   
+      
