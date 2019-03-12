3ee4ceba2bc33d6245f97f59cd025e68f295fa25
Gago <cleamen@free.fr>
2019-03-12 14:07:08 +0100

    Text Manager

---
+++ Assets/Scripts/CA_Scripts/EgoSoundToPlay.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EgoSoundToPlay : MonoBehaviour {
+
+    [SerializeField]
+    AudioManager audioManager;
+
+    [SerializeField]
+    AudioSource audioSource;
+
+    [SerializeField]
+    DialogManager dialogManager;
+
+    [SerializeField]
+    private bool triggered = false;
+
+    [Header("The ID referencing the precise voice line to use")]
+    [SerializeField]
+    private int ID;
+
+    public Dialog dialog;
+
+    private void Update()
+    {
+        if (triggered == true)
+        {
+            if (!audioSource.isPlaying)
+            {
+                FindObjectOfType<DialogManager>().EndDialog();
+                Destroy(this);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && triggered == false)
+        {
+            triggered = true;
+            FindObjectOfType<DialogManager>().StartDialog(dialog);
+            audioManager.EgoDialog(ID);
+            audioSource.clip = audioManager.soundToPlay;
+            audioSource.Play();
+        }
+    }
+
+}
